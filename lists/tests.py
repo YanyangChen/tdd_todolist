4b82cd9edfc61408cd420b772572af6c24f289e9
YanyangChen <184536632@qq.com>
2019-12-11 15:41:26 +0800

    new lists for users

--- lists/tests.py
+++ lists/tests.py
@@ -39,7 +39,9 @@ class HomePageTest(TestCase):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
+        #self.assertIn(row_text, [row.text for row in rows])
         self.assertIn(row_text, [row.text for row in rows])
+        return
     
     def test_saving_and_retrieving_items(self):
         first_item = Item()
