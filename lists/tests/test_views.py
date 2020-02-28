966fd18a4b86412f458dec52ea3e2153098f52c8
chenyanyang <184536632@qq.com>
2020-02-28 18:57:50 +0800

    page 289

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,6 +7,12 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 from django.utils.html import escape
 from lists.forms import EMPTY_ITEM_ERROR
+from unittest import skip
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
+)
+
 # Create your tests here.
 
 
@@ -105,6 +111,7 @@ class NewListTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
+        
         self.client.post(
             f'/lists/{correct_list.id}/',
             data={'text': 'A new item for an existing list'}
@@ -147,7 +154,8 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        # self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
     
     def post_invalid_input(self):
@@ -168,8 +176,21 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
